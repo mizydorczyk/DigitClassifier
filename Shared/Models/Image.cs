f5d62219b9e4394a9e20fe8f17b4a3a20b337ab5
Mateusz Izydorczyk <matizydorczyk@outlook.com>
2024-02-01 20:14:58 +0100

    feat: add image class

---
+++ Shared/Models/Image.cs
@@ -0,0 +1,36 @@
+﻿namespace Shared.Models
+{
+    public class Image
+    {
+        private readonly byte[] _pixels;
+        private readonly int _width;
+        private readonly int _height;
+
+        public int Label { get; private set; }
+        public Image(int label, byte[] pixels, int width, int height)
+        {
+            Label = label;
+
+            _pixels = pixels;
+            _width = width;
+            _height = height;
+        }
+
+        public byte this[int x, int y]
+        {
+            get
+            {
+                if (x < 0 || y < 0)
+                    throw new ArgumentOutOfRangeException("X and Y must be greater than 0.");
+
+                if (x >= _width)
+                    throw new ArgumentOutOfRangeException($"X must be lower than {_width}.");
+
+                if (y >= _height)
+                    throw new ArgumentOutOfRangeException($"Y must be lower than {_height}.");
+
+                return _pixels[x + _width * y]; 
+            }
+        }
+    }
+}
